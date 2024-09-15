c8c5ef17156e9805e9221d22b4aaf6292314ded2
ReadWriteV <ReadWriteV@outlook.com>
2024-09-15 19:21:58 +0800

    add calc example

---
+++ calc/main.cpp
@@ -0,0 +1,17 @@
+#include "scanner.hpp"
+#include "parser.hpp"
+
+#include <print>
+
+int main(int argc, char const *argv[])
+{
+    bool debug_flag = (argc == 2) && (std::strcmp(argv[1], "-d") == 0);
+
+    yy::Scanner scanner{std::cin, std::cout};
+    scanner.set_debug(debug_flag);
+
+    yy::Parser parser{scanner};
+    parser.set_debug_level(debug_flag);
+
+    return parser();
+}
